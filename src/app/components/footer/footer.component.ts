c8656e2746ed1e970befa44b484dd1a50bccc97d
mAbduqayum <mabduqayum@gmail.com>
2022-06-11 19:41:34 +0500

    worked on main page

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
   selectedValue: string | undefined;
-  value= 'Enter your email';
+  value: string | undefined;
 
   constructor() { }
 
