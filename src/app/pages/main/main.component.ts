8abedfc53645b713516229d686b0b73cf8b8d2c4
mAbduqayum <mabduqayum-VEL@transparent.com>
2022-07-25 14:50:25 +0500

    Worked on main page

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,12 +1,25 @@
 import {Component, HostListener, OnInit} from '@angular/core';
+import {animate, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss'],
+  animations: [
+    trigger('fab-animation', [
+      transition(':enter', [
+        style({opacity: 0}),
+        animate('300ms', style({opacity: 1})),
+      ]),
+      transition(':leave', [
+        animate('150ms', style({opacity: 0})),
+      ]),
+    ]),
+  ]
 })
 export class MainComponent implements OnInit {
   showFAB = false;
+  previousScrollTop = 0;
 
   constructor() {
   }
@@ -20,6 +33,7 @@ export class MainComponent implements OnInit {
 
   @HostListener('window:scroll')
   onScrollChange(): void {
-    this.showFAB = window.scrollY > 75;
+    this.showFAB = window.scrollY < this.previousScrollTop && window.scrollY > 75;
+    this.previousScrollTop = window.scrollY;
   }
 }
